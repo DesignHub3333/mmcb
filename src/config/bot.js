7c4084baff17449d1c874806b839a93c90eb831e
DesignHub3333 <syruspluxury@gmail.com>
2026-08-05 18:08:44 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Hey Wasuppp <<<3",     // this is what people actually see
+        state: "🛡️ Standing guard 24/7.",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
